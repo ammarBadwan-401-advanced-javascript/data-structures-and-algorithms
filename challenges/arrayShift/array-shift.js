801414f28e30bd47188c6dd23f8a779cf1d16ad0
Ammar <ammar.badwan95@gmail.com>
2020-05-19 01:00:24 +0300

    finished the challenge, the test and the readme

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,11 +1,11 @@
 'use strict';
-function insertShiftArray(arr,number){
+function insertShiftArray(arr,value){
   let middleIndex = arr.length/2;
   let newArr = [];
   for (let i=0;i<middleIndex;i++){
     newArr[newArr.length] = arr[i];
   }
-  newArr[newArr.length] = number;
+  newArr[newArr.length] = value;
   for (let q = newArr.length-1;q<arr.length;q++){
     newArr[newArr.length]= arr[q];
   }
