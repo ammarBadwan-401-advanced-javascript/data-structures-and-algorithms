242b78357a62f36686411f3fbfbc99a53b0ff35c
Ammar <ammar.badwan95@gmail.com>
2020-06-11 17:42:19 +0300

    finished setting up main files and classes

---
+++ Data-Structures/tree/tree.js
@@ -0,0 +1,45 @@
+'use strict';
+
+class Node {
+  constructor(){
+    this.value;
+    this.left;
+    this.right;
+  }
+}
+
+class BinaryTree {
+  constructor(){
+    
+  }
+
+  preOrder(){
+    
+  }
+
+  inOrder(){
+    
+  }
+
+  postOrder(){
+    
+  }
+}
+
+
+
+class BinarySearchTree {
+  constructor(){
+        
+  }
+
+  add(value){
+
+  }
+
+  contains(value){
+    
+  }
+}
+
+module.exports = {BinaryTree,BinarySearchTree};
