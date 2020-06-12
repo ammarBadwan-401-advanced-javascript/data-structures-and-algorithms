803425646e166bd6aac9c6f72ce9ba9d99da4243
Ammar <ammar.badwan95@gmail.com>
2020-06-13 01:54:31 +0300

    finished building binary tree and binary search tree

--- Data-Structures/tree/tree.js
+++ Data-Structures/tree/tree.js
@@ -1,44 +1,96 @@
 'use strict';
 
 class Node {
-  constructor(){
-    this.value;
-    this.left;
-    this.right;
+  constructor(value, left = null, right = null){
+    this.value = value;
+    this.left = left;
+    this.right = right;
   }
 }
 
 class BinaryTree {
-  constructor(){
-    
+  constructor(root){
+    this.root = root;
   }
 
   preOrder(){
-    
+    let results = [];
+
+    let _walk = (node)=>{
+      results.push(node.value);
+      if(node.left) _walk(node.left);
+      if (node.right) _walk(node.right);
+    };
+    _walk(this.root);
+    return results;
   }
 
   inOrder(){
-    
+    let results = [];
+
+    let _walk = (node)=>{
+      if(node.left) _walk(node.left);
+      results.push(node.value);
+      if (node.right) _walk(node.right);
+    };
+    _walk(this.root);
+    return results;
   }
 
   postOrder(){
-    
+    let results = [];
+
+    let _walk = (node)=>{
+      if(node.left) _walk(node.left);
+      if (node.right) _walk(node.right);
+      results.push(node.value);
+    };
+    _walk(this.root);
+    return results;
   }
 }
 
-
-
 class BinarySearchTree {
-  constructor(){
-        
+  constructor(root){
+    this.root = root;
   }
 
   add(value){
-
+    let newNode = new Node(value);
+    let addition = (node) => {
+      if(value < node.value){
+        if(node.left){
+          addition(node.left);
+        } else { node.left = newNode;}
+      } else if (value > node.value){
+        if(node.right){
+          addition(node.right);
+        } else (node.right = newNode);
+      }
+    };
+    addition(this.root);
+    return this;
   }
 
   contains(value){
+    let answer = false;
     
+    let search = (node) => {
+      if(value === node.value){
+        answer = true;
+        return;
+      } else if (value < node.value){
+        if(node.left){
+          search(node.left);
+        }
+      } else if (value > node.value){
+        if(node.right){
+          search(node.right);
+        }
+      }
+    };
+    search(this.root);
+    return answer;
   }
 }
 
