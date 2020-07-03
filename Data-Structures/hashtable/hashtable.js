044f3a8eda6f94374074da012f3f7f82d5bfb5a8
Ammar <ammar.badwan95@gmail.com>
2020-07-03 18:42:27 +0300

    finished adding initial files and buildup for hashtable e implementation

---
+++ Data-Structures/hashtable/hashtable.js
@@ -0,0 +1,12 @@
+'use strict';
+const LinkedList = require('./linkedlist');
+
+class Hashmap {
+  constructor(size){
+    this.size = size;
+    this.map = new Array(size);
+  }
+
+}
+
+module.exports = Hashmap;
