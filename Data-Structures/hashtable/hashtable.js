7ad73892e2e6a0cc53033e2a1c640393596014e9
Ammar <ammar.badwan95@gmail.com>
2020-07-03 21:47:59 +0300

    finished adding hash,add and get methods of hashtable

--- Data-Structures/hashtable/hashtable.js
+++ Data-Structures/hashtable/hashtable.js
@@ -7,6 +7,29 @@ class Hashmap {
     this.map = new Array(size);
   }
 
+  hash(key){
+    return key.split('').reduce((acc,val)=>{
+      return acc + val.charCodeAt();
+    },0)* 599 % this.size;
+  }
+
+  add(key,value){
+    let hash = this.hash(key);
+    if(!this.map[hash]){
+      this.map[hash] = new LinkedList();
+    }
+    let entry = {[key]:value};
+    this.map[hash].add(entry);
+  }
+
+  get(key){
+    let hash = this.hash(key);
+    if (!this.map[hash]){
+      return null;
+    } else {
+      return this.map[hash].head.value;
+    }
+  }
 }
 
 module.exports = Hashmap;
